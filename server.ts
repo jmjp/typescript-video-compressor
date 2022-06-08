bf7f6d50ef89362bf7532f2d60934bc53956c6b4
João Miguel <joaozinhosleepy@gmail.com>
2022-06-08 09:17:10 -0300

    update

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import app from './src/app';
 
 app.listen(process.env.PORT || 8080, () => {
-    console.log(`⚡ Rodando em ${process.env.PORT || 8080}`);
+    console.log(`⚡Rodando em ${process.env.PORT || 8080}`);
 })
