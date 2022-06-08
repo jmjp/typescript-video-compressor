658bbc29ce7d57d1e45c56caf73b8f7e8a907834
João Miguel <joaozinhosleepy@gmail.com>
2022-06-08 09:29:33 -0300

    async upload

--- src/controllers/UploadController.ts
+++ src/controllers/UploadController.ts
@@ -7,45 +7,49 @@ import { v4 as uuidv4 } from 'uuid';
 class UploadController {
     async handle(req: Request, res: Response) {
         const uuid = uuidv4();
-        try{
-            let file = req.file;
-            let filePath = file.path;
-            let fileExtension = file.originalname.split('.').pop();
-            await ffmpeg(filePath).output(`${filePath}.${fileExtension}`).size('50%').aspectRatio('16:9').FPS(30).format('mp4')
-            .on('end',async (stdout,stderr) => {
-                if(stderr){
-                    console.log(stderr);
-                }
-                const upload = await bucket.upload({
-                    file: fs.createReadStream(filePath + '.mp4'),
-                    fileName: `${uuid}.${fileExtension}`,
-                    folder: 'videos',
-                    useUniqueFileName: true
-                })
-                fs.unlink(filePath, (err) => {
-                    if(err){
-                        console.log(err);
-                    }
-                    console.log("input deletado");
-                })
-                fs.unlink(`${filePath}.${fileExtension}`, (err) => {
-                    if(err){
-                        console.log(err);
-                    }
-                    console.log("output deletado");
-                })
-                return res.json(upload);
+        upload(req.file,uuid);
+        return res.json({message:"uploaded"});
+    }
+}
+
+async function upload(file: Express.Multer.File, uuid: string) {
+    try{
+        let filePath = file.path;
+        let fileExtension = file.originalname.split('.').pop();
+        await ffmpeg(filePath).output(`${filePath}.${fileExtension}`).size('50%').aspectRatio('16:9').FPS(30).format('mp4')
+        .on('end',async (stdout,stderr) => {
+            if(stderr){
+                console.log(stderr);
+            }
+            const upload = await bucket.upload({
+                file: fs.createReadStream(filePath + '.mp4'),
+                fileName: `${uuid}.${fileExtension}`,
+                folder: 'videos',
+                useUniqueFileName: true
             })
-            .on('error', (stdout, stderr) => {
-                if(stderr){
-                    console.log(stderr);
+            fs.unlink(filePath, (err) => {
+                if(err){
+                    console.log(err);
                 }
-                return res.status(400).json({message: 'Falha ao enviar arquivo'});
+                console.log("input deletado");
             })
-            .run();
-        }catch(error){
-            return res.status(400).json({message: error.message});
-        }
+            fs.unlink(`${filePath}.${fileExtension}`, (err) => {
+                if(err){
+                    console.log(err);
+                }
+                console.log("output deletado");
+            })
+            console.log(upload);
+        })
+        .on('error', (stdout, stderr) => {
+            if(stderr){
+                console.log(stderr);
+            }
+            console.log('Falha ao enviar arquivo');
+        })
+        .run();
+    }catch(error){
+        console.log(error)
     }
 }
 
